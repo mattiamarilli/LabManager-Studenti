c8345c5a35a0050863b9a1bf94b3f82ea750efbf
mattiamarilli <mattiamarilli@gmail.com>
2019-04-19 19:45:36 +0200

    added worjing scan button on bashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {ClassmatesService} from '../services/classmates.service';
-import {Membro,Utensile} from '../model'
+import {Membro, Utensile} from '../model'
 import {GroupService} from '../services/group.service'
+import { QrService } from '../services/qr.service';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -9,19 +10,27 @@ import {GroupService} from '../services/group.service'
 })
 export class DashboardComponent implements OnInit {
 
-  constructor(private classmateService:ClassmatesService,private groupService:GroupService) { }
+  constructor(private classmateService: ClassmatesService,private groupService: GroupService, private qrService: QrService) { }
 
-  compagni:Membro[];
-  utensiliInUso:Utensile[];
+  compagni: Membro[];
+  utensiliInUso: Utensile[];
+  output: string;
 
   updateToolAndMates(){
-    this.groupService.getMembri().subscribe((data:Membro[])=>this.compagni = data);
+    this.groupService.getMembri().subscribe((data: Membro[])=>this.compagni = data);
     this.classmateService.getInUseTools().subscribe((data:Utensile[])=>this.utensiliInUso = data);
   }
 
   ngOnInit() {
       this.groupService.getMembri().subscribe((data:Membro[])=>this.compagni = data);
       this.classmateService.getInUseTools().subscribe((data:Utensile[])=>this.utensiliInUso = data);
+  }
+
+  onFileChange(event) {
+    const file = event.target.files[0];
+    this.qrService.scanFile(file).subscribe(data => {
+      this.output = data;
+    });
   }
 
 }
