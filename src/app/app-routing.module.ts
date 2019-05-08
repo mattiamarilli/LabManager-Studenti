704591e4e832fbe2abb3a877d0793e02990f3e1f
mattiamarilli <mattiamarilli@gmail.com>
2019-05-08 14:26:55 +0200

    some mod

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import {QrReaderComponent} from './qr-reader/qr-reader.component'
 import { LoginComponent } from './login/login.component';
 import {DashboardComponent} from './dashboard/dashboard.component';
 import { AuthGuard } from './_guards/auth.guard';
+import {UserComponent} from './user/user.component'
 
 
 const routes: Routes = [
   { path: 'login',component:LoginComponent},
   { path: 'scan',component:QrReaderComponent,canActivate: [AuthGuard]},
   { path: '', redirectTo: '/scan', pathMatch: 'full'},
+  { path: 'user',component:UserComponent},
   { path: 'dashboard', component: DashboardComponent,},
 ];
 
